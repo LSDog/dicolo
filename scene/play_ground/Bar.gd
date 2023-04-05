fd1287478bd6e3953deb812aa95aa078a487bac4
LSDog <2509863159@qq.com>
2023-04-05 18:38:03 +0800

    + two "bar" on the two side of tracks

---
+++ scene/play_ground/Bar.gd
@@ -0,0 +1,15 @@
+@tool
+extends Path2D
+
+var slash_right_texture := preload("res://image/texture/slash_right.svg");
+var width_curve := preload("res://scene/play_ground/bar_width_curve.tres");
+
+func _ready() -> void:
+	var line := Line2D.new();
+	line.width = 30;
+	line.width_curve = width_curve;
+	line.points = curve.get_baked_points();
+	line.texture_mode = Line2D.LINE_TEXTURE_TILE;
+	line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED;
+	line.texture = slash_right_texture;
+	add_child(line);
