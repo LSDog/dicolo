40e0b570d56c7cb63d09536628326aa4b17de2b7
LSDog <2509863159@qq.com>
2023-06-29 00:38:22 +0800

    more settings

--- scene/setting/Setting.gd
+++ scene/setting/Setting.gd
@@ -6,15 +6,27 @@ const COLOR_NORMAL := Color.WHITE;
 const COLOR_WAITING := Color("e098ff");
 const COLOR_CHANGED := Color("ffdd00");
 
-@onready var button_FullScreen := $Panel/Scroll/List/Video/FullScreen/Button;
-@onready var option_FullScreenMode := $Panel/Scroll/List/Video/FullScreenMode/Option;
+# Video #
+@onready var button_FullScreen := $Panel/Scroll/Margin/List/Video/FullScreen/Button;
+@onready var option_FullScreenMode := $Panel/Scroll/Margin/List/Video/FullScreenMode/Option;
 var option_FullScreenMode_items = [DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, DisplayServer.WINDOW_MODE_FULLSCREEN];
-@onready var input_FPS := $Panel/Scroll/List/Video/FPS/Input;
-@onready var button_VSync := $Panel/Scroll/List/Video/VSync/Button;
-@onready var label_Scale := $Panel/Scroll/List/Video/Scale/Label;
-@onready var slider_Scale := $Panel/Scroll/List/Video/Scale/HSlider;
-@onready var label_Gamepad := $Panel/Scroll/List/Control/Gamepad/Label;
-@onready var option_Gamepad := $Panel/Scroll/List/Control/Gamepad/Option;
+@onready var input_FPS := $Panel/Scroll/Margin/List/Video/FPS/Input;
+@onready var button_VSync := $Panel/Scroll/Margin/List/Video/VSync/Button;
+@onready var label_Scale := $Panel/Scroll/Margin/List/Video/Scale/Label;
+@onready var slider_Scale := $Panel/Scroll/Margin/List/Video/Scale/HSlider;
+# Audio #
+@onready var slider_volumeMaster := $Panel/Scroll/Margin/List/Audio/VolumeMaster/HSlider;
+@onready var slider_volumeMusic := $Panel/Scroll/Margin/List/Audio/VolumeMusic/HSlider;
+@onready var slider_volumeEffect := $Panel/Scroll/Margin/List/Audio/VolumeEffect/HSlider;
+@onready var slider_volumeVoice := $Panel/Scroll/Margin/List/Audio/VolumeVoice/HSlider;
+# Device #
+@onready var label_Gamepad := $Panel/Scroll/Margin/List/Device/Gamepad/Label;
+@onready var option_Gamepad := $Panel/Scroll/Margin/List/Device/Gamepad/Option;
+# GamePlay #
+var audio_offset :int = 0;
+@onready var label_AudioOffset := $Panel/Scroll/Margin/List/Gameplay/AudioOffset/LabelOffset;
+@onready var button_AudioOffsetAdd := $Panel/Scroll/Margin/List/Gameplay/AudioOffset/ButtonAdd;
+@onready var button_AudioOffsetSub := $Panel/Scroll/Margin/List/Gameplay/AudioOffset/ButtonSub;
 
 var loading_data := true;
 
@@ -22,12 +34,12 @@ func _ready():
 	
 	DataManager.load_data(DataManager.DATA_TYPE.SETTING);
 	
-	set_setting_from_data();
-	
 	bind_gui_action();
 	
+	set_setting_from_data();
+	
 
-
+## 绑定gui与设置变化
 func bind_gui_action():
 	
 	button_FullScreen.toggled.connect(func(pressed):
@@ -38,13 +50,13 @@ func bind_gui_action():
 			if !pressed else
 			option_FullScreenMode_items[id]
 		);
-		set_data("FullScreen", pressed);
+		save_data("FullScreen", pressed);
 	);
 	option_FullScreenMode.item_selected.connect(func(index: int):
 		if button_FullScreen.button_pressed:
 			print("FullScreen -> ", option_FullScreenMode.get_item_text(index), ": ", button_FullScreen.button_pressed);
 			DisplayServer.window_set_mode(option_FullScreenMode_items[index]);	
-		set_data("FullScreenMode", index);
+		save_data("FullScreenMode", index);
 	);
 	input_FPS.text_changed.connect(func(_text: String):
 		input_FPS.modulate = COLOR_WAITING;
@@ -57,7 +69,7 @@ func bind_gui_action():
 			# 给初始化加载数据用的
 			if input_FPS.text != text:
 				input_FPS.text = text;
-			set_data("FPS", text);
+			save_data("FPS", text);
 	);
 	button_VSync.toggled.connect(func(pressed: bool):
 		DisplayServer.window_set_vsync_mode(
@@ -65,7 +77,7 @@ func bind_gui_action():
 			if pressed else
 			DisplayServer.VSYNC_DISABLED
 		);
-		set_data("VSync", pressed);
+		save_data("VSync", pressed);
 	);
 	slider_Scale.value_changed.connect(func(value: float):
 		get_tree().root.content_scale_factor = value;
@@ -74,7 +86,23 @@ func bind_gui_action():
 		event.set_button_index(MOUSE_BUTTON_LEFT);
 		event.set_pressed(false);
 		Input.parse_input_event(event);
-		set_data("Scale", value);
+		save_data("Scale", value);
+	);
+	slider_volumeMaster.value_changed.connect(func(value: float):
+		set_audio_bus_volume("Master", value);
+		save_data("Volume.Master", value);
+	);
+	slider_volumeMusic.value_changed.connect(func(value: float):
+		set_audio_bus_volume("Music", value);
+		save_data("Volume.Music", value);
+	);
+	slider_volumeEffect.value_changed.connect(func(value: float):
+		set_audio_bus_volume("Effect", value);
+		save_data("Volume.Effect", value);
+	);
+	slider_volumeVoice.value_changed.connect(func(value: float):
+		set_audio_bus_volume("Voice", value);
+		save_data("Volume.Voice", value);
 	);
 	update_gamepad_select();
 	Input.joy_connection_changed.connect(func(device: int, connected: bool):
@@ -91,6 +119,20 @@ func bind_gui_action():
 		Global.gamepad_id = id;
 		update_label_Gamepad();
 	);
+	var music_player = Global.scene_MainMenu.music_player;
+	music_player.beat.connect(func():
+		label_AudioOffset.create_tween().tween_property(
+			label_AudioOffset, "modulate:a", 1.0, 60.0/music_player.bpm
+		).from(0.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC);
+	);
+	button_AudioOffsetAdd.pressed.connect(func():
+		set_audio_offset(audio_offset + 1);
+		save_data("AudioOffset", audio_offset);
+	);
+	button_AudioOffsetSub.pressed.connect(func():
+		set_audio_offset(audio_offset - 1);
+		save_data("AudioOffset", audio_offset);
+	);
 
 func set_setting_from_data():
 	
@@ -99,6 +141,13 @@ func set_setting_from_data():
 	input_FPS.text_submitted.emit(get_data("FPS", 60));
 	button_VSync.button_pressed = get_data("VSync", button_VSync.button_pressed);
 	slider_Scale.value = get_data("Scale", slider_Scale.value);
+	
+	slider_volumeMaster.value = get_data("Volume.Master", slider_volumeMaster.value);
+	slider_volumeMusic.value = get_data("Volume.Music", slider_volumeMusic.value);
+	slider_volumeEffect.value = get_data("Volume.Effect", slider_volumeEffect.value);
+	slider_volumeVoice.value = get_data("Volume.Voice", slider_volumeVoice.value);
+	
+	set_audio_offset(get_data("AudioOffset", 0));
 	
 	loading_data = false;
 	Global.data_loaded_setting.emit();
@@ -119,16 +168,44 @@ func update_gamepad_select():
 func update_label_Gamepad():
 	label_Gamepad.text = "Gamepad: " + str(Global.gamepad_id+1) + "/" + str(Global.gamepad_count);
 
+func set_audio_bus_volume(bus: String, volume_db: float):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), volume_db);
+
+func get_audio_offset() -> int:
+	return audio_offset;
+
+func set_audio_offset(offset: int):
+	if offset < 0: offset = 0;
+	audio_offset = offset;
+	label_AudioOffset.text = ("+" if offset >= 0 else "") + str(offset) + " ms";
+	var delay := AudioServer.get_bus_effect(0, 0) as AudioEffectDelay;
+	delay.tap1_delay_ms = offset;
+
 func _input(event: InputEvent):
 	if visible && event is InputEventMouseButton:
 		if !get_rect().has_point(event.position):
 			accept_event();
+			# 点到外面就收回
 			if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-				visible = false;
+				anim_hide();
 	elif event.is_action_pressed("full_screen"):
 		button_FullScreen.button_pressed = !button_FullScreen.button_pressed;
 
-func set_data(key: String, value):
+func anim_show():
+	visible = true;
+	var tween = create_tween();
+	tween.tween_property(self, "offset_right", 0, 0.15
+		).from(size.x).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO);
+
+func anim_hide():
+	var tween = create_tween();
+	tween.tween_property(self, "offset_right", size.x, 0.15
+		).from(0.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO);
+	tween.finished.connect(func(): visible = false);
+	
+## 保存数据到配置文件
+func save_data(key: String, value):
+	if loading_data: return;
 	DataManager.data_setting[key] = value;
 	DataManager.save_data(DataManager.DATA_TYPE.SETTING);
 
